5b00968473ade90d985b8842f922873eaea837a7
door7302 <droy@juniper.net>
2023-10-03 15:33:18 +0200

    fix

--- html/assets/js/doc.js
+++ html/assets/js/doc.js
@@ -6,32 +6,40 @@ function updateDoc() {
     var graf = document.getElementById("profileGraf");
     var kapa = document.getElementById("profileKapa");
 
+    if (p == "default") {
+      img.setAttribute('src', "img/default.png");
+      desc.innerHTML= "N/A";
+      tele.innerHTML= "N/A";
+      graf.innerHTML= "N/A";
+      kapa.innerHTML= "N/A";
+    }
+    else {
+      var dataToSend = {"profile": p};
+      // send data
+      $(function() {
+          $.ajax({
+              type: 'POST',
+              url: "/updatedoc",
+              data: JSON.stringify(dataToSend),
+              contentType: "application/json",
+              dataType: "json",
+              success : function(json) {
+                if (json.status == "OK") {
+                  img.setAttribute('src', "img/" +json.img);
+                  desc.innerHTML= json.desc;
+                  tele.innerHTML= json.tele;
+                  graf.innerHTML= json.graf;
+                  kapa.innerHTML= json.kapa;
 
-    var dataToSend = {"profile": p};
-    // send data
-    $(function() {
-        $.ajax({
-            type: 'POST',
-            url: "/updatedoc",
-            data: JSON.stringify(dataToSend),
-            contentType: "application/json",
-            dataType: "json",
-            success : function(json) {
-              if (json.status == "OK") {
-                img.setAttribute('src', json.img);
-                desc.innerHTML= json.desc;
-                tele.innerHTML= json.tele;
-                graf.innerHTML= json.graf;
-                kapa.innerHTML= json.kapa;
-
+                }
+                else {
+                  alertify.alert("JSTO...", json.msg);
+                }             
+              },    
+              error : function(xhr, ajaxOptions, thrownError) {        
+                  alertify.alert("JSTO...", "Unexpected error");
               }
-              else {
-                alertify.alert("JSTO...", json.msg);
-              }             
-            },    
-            error : function(xhr, ajaxOptions, thrownError) {        
-                alertify.alert("JSTO...", "Unexpected error");
-            }
-        });
-    });
+          });
+      });
+  }
 }
