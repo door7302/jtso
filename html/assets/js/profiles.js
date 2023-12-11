090a1c31d21e56b37649d22f23f47c127cfaece7
door7302 <droy@juniper.net>
2023-12-11 16:33:37 +0100

    fix

--- html/assets/js/profiles.js
+++ html/assets/js/profiles.js
@@ -10,36 +10,40 @@ function addAsso() {
           raw_selected = raw_selected + option.value + " "
       }
   }
- 
   
-  var dataToSend = {"shortname": r, "profiles": selected};
-  waitingDialog.show();
-  // send data
-  $(function() {
-      $.ajax({
-          type: 'POST',
-          url: "/addprofile",
-          data: JSON.stringify(dataToSend),
-          contentType: "application/json",
-          dataType: "json",
-          success : function(json) {
-            if (json.status == "OK") {
-              var row = $("<tr><td>"+r+"</td><td>"+raw_selected+'</td><td class="d-xxl-flex justify-content-xxl-center"><button onclick="removeAsso("'+r+'", this)" class="btn btn-danger" style="margin-left: 5px;" type="submit"><i class="fa fa-trash" style="font-size: 15px;"></i></button></td></tr>')
-              $("#ListRtrs").append(row);
-              alertify.success("Profile(s) have been successfulfy added to router "+r)
-              waitingDialog.hide();
+  if (selected.length == 0) {
+    alertify.alert("JSTO...", "Please select at least one Profile in the list.");
+  }
+  else {
+    var dataToSend = {"shortname": r, "profiles": selected};
+    waitingDialog.show();
+    // send data
+    $(function() {
+        $.ajax({
+            type: 'POST',
+            url: "/addprofile",
+            data: JSON.stringify(dataToSend),
+            contentType: "application/json",
+            dataType: "json",
+            success : function(json) {
+              if (json.status == "OK") {
+                var row = $("<tr><td>"+r+"</td><td>"+raw_selected+'</td><td class="d-xxl-flex justify-content-xxl-center"><button onclick="removeAsso("'+r+'", this)" class="btn btn-danger" style="margin-left: 5px;" type="submit"><i class="fa fa-trash" style="font-size: 15px;"></i></button></td></tr>')
+                $("#ListRtrs").append(row);
+                alertify.success("Profile(s) have been successfulfy added to router "+r)
+                waitingDialog.hide();
+              }
+              else {
+                alertify.alert("JSTO...", json.msg);
+                waitingDialog.hide();
+              }             
+            },    
+            error : function(xhr, ajaxOptions, thrownError) {        
+                alertify.alert("JSTO...", "Unexpected error");
+                waitingDialog.hide();
             }
-            else {
-              alertify.alert("JSTO...", json.msg);
-              waitingDialog.hide();
-            }             
-          },    
-          error : function(xhr, ajaxOptions, thrownError) {        
-              alertify.alert("JSTO...", "Unexpected error");
-              waitingDialog.hide();
-          }
-      });
-  });
+        });
+    });
+}
 }
 
 function removeAsso(name, td) { 
