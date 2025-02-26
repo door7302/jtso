c618550dc04b5b247e89636816b2c127c2f8585f
door7302 <droy@juniper.net>
2025-02-26 15:48:14 +0100

    version 1.0.9

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const JTSO_VERSION string = "1.0.5"
+const JTSO_VERSION string = "1.0.9"
 
 var SUPPORTED_DEVICE = []string{"MX", "PTX", "ACX", "EX", "QFX", "SRX", "CRPD", "CPTX", "VMX", "VSRX", "VJUNOS", "VEVO"}
 
