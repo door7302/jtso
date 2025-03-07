50d15266ffac7082a7a85eeaf6d9e03c475c5cae
door7302 <droy@juniper.net>
2025-03-07 17:24:00 +0100

    fix

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 )
 
 // this is config displayed on the main page
-const JTSO_VERSION string = "1.0.12"
+const JTSO_VERSION string = "1.0.13"
 
 type PortalConfig struct {
 	Https          bool
