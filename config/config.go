1d88eafc403787bc3e82bdb23cf484dd3e946e94
droy <droy@juniper.net>
2025-12-11 18:43:52 +0100

    upgrade docker api

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 )
 
 // this is config displayed on the main page
-const JTSO_VERSION string = "1.0.18"
+const JTSO_VERSION string = "1.0.19"
 
 type PortalConfig struct {
 	Https          bool
